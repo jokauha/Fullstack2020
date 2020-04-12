af2e15fa89bc8419acac923d0daf19af0826d548
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-04-12 21:56:17 +0300

    unicafe step4

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -2,17 +2,25 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good+neutral+bad}</p>
-      <p>average {(good-bad)/(good+neutral+bad)}</p>
-      <p>positive {(good/(good+neutral+bad))*100} %</p>
-    </div>
-  )
+  if (good+neutral+bad === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good+neutral+bad}</p>
+        <p>average {(good-bad)/(good+neutral+bad)}</p>
+        <p>positive {(good/(good+neutral+bad))*100} %</p>
+      </div>
+    )
+  }
 }
 
 const App = () => {
