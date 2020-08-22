75348666c8d113802cc1e0ab62f1a29aafc3aaff
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-08-22 16:42:49 +0200

    2.14*: maiden tiedot, step3

--- osa2/maidentiedot/src/components/Country.js
+++ osa2/maidentiedot/src/components/Country.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Country = ({ country }) => {
+
+    const [ weather, setWeather ] = useState([])
+    const api_key = process.env.REACT_APP_API_KEY
+    const api_request = 'http://api.weatherstack.com/current?access_key=' + api_key + '&query=' + country.capital
+
+    useEffect(() => {
+        axios
+          .get(api_request)
+          .then(response => {
+              console.log(response.data.current)
+              setWeather(response.data.current)
+          })
+      }, [api_request])
+
     console.log(country)
     return (
         <div>
@@ -16,6 +31,11 @@ const Country = ({ country }) => {
                 })}
             </ul>
             <img src={country.flag} alt="flag" width="200"></img>
+            <h2>Weather in {country.capital}</h2>
+            <p>temperature: {weather.temperature} Celsius</p>
+            <img src={weather.weather_icons} alt="weather_icon" width="50"></img>
+            <p>wind: {weather.wind_speed} mph direction {weather.wind_dir}</p>
+
         </div>
     )
 }
