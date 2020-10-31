278158e70b809da13d406ed171cb9c869c81760f
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-10-31 21:16:37 +0200

    4.5*: apufunktioita ja yksikkötestejä, step3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -5,13 +5,22 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    const likesArray = blogs.map( blog => blog.likes )
     const reducer = (acc, cur) => acc + cur
+    const likesArray = blogs.map( blog => blog.likes )
 
     return likesArray.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const likesArray = blogs.map( blog => blog.likes )
+    const maxLikes = Math.max.apply(Math, likesArray)
+    const favoriteBlog = blogs[likesArray.indexOf(maxLikes)]
+    const returnInfo = (({ title, author, likes }) => ({ title, author, likes }))(favoriteBlog)
+    return returnInfo
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
