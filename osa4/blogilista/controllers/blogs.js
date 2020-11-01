f2dd1ffb4947993e04fb72f813eb98d3f2f4777a
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-11-02 01:36:06 +0200

    4.8: blogilistan testit, step 1

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,24 +1,20 @@
 const blogsRouter = require('express').Router()
-const blog = require('../models/blog')
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
 })
-  
+
 blogsRouter.post('/', (request, response, next) => {
     const blog = new Blog(request.body)
-  
+
     blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-      .catch(error => next(error))
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+        .catch(error => next(error))
 })
 
 module.exports = blogsRouter
