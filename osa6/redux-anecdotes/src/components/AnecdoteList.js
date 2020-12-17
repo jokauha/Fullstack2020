b04cb94cfc572a449680ee61c8feeef99c5238c9
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-12-17 19:32:12 +0200

    6.9: anekdootit, step7

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -8,7 +8,6 @@ const AnecdoteList = () => {
 
     return (
         <div>
-            <h2>Anecdotes</h2>
             {anecdotes
                 .sort((a, b) => b.votes - a.votes )
                 .map(anecdote =>
