0dec1ee58b9c4916dffaec3cfa17315e82bc36b5
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-12-19 17:37:20 +0200

    6.21: anekdootit, loppuhuipennus

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -3,19 +3,19 @@ import { useSelector } from 'react-redux'
 
 const Notification = () => {
   const notification = useSelector(state => state.notification)
-
+  console.log(notification)
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
 
-  if (notification === null) {
+  if (notification.notification === null) {
     return null
   } else {
     return (
       <div style={style}>
-        {notification}
+        {notification.notification}
       </div>
     )
   }
--- osa6/redux-anecdotes/src/reducers/notificationReducer.js
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,26 +1,39 @@
-const notificationReducer = (state = null, action) => {
+const initialState = {
+    notification: null,
+    timeoutID: null
+}
+const notificationReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'SET_NOTIFICATION':
-            return action.notification
+            return {
+                notification: action.notification,
+                timeoutID: action.timeoutID
+            }
         case 'REMOVE_NOTIFICATION':
-            return null
+            return {
+                notification: null,
+                timeoutID: null,
+            }
         default:
             return state
     }
 }
 
 export const setNotification = (content, time) => {
-    return async dispatch => {
+    return async (dispatch, getState) => {
         const millis = time*1000
-        dispatch({
-            type: 'SET_NOTIFICATION',
-            notification: content,
-        })
-        setTimeout(() => {
+        const currentState = getState()
+        clearTimeout(currentState.notification.timeoutID);
+        const timeoutID = setTimeout(() => {
             dispatch({
                 type: 'REMOVE_NOTIFICATION'
             })
         }, millis)
+        dispatch({
+            type: 'SET_NOTIFICATION',
+            notification: content,
+            timeoutID: timeoutID,
+        })
     }
 }
 
