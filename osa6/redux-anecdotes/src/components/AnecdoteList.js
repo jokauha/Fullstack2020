7b05863c94bbcec66902866980844d9f81cd1fdf
Joakim Kauhanen <joakim.kauhanen@gmail.com>
2020-12-19 17:05:51 +0200

    6.20: anekdootit ja connect, step2

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -42,8 +42,7 @@ const mapDispatchToProps = {
     setNotification,
 }
 
-const ConnectedAnecdotes = connect(
+export default connect(
     mapStateToProps,
     mapDispatchToProps
 )(AnecdoteList)
-export default ConnectedAnecdotes
